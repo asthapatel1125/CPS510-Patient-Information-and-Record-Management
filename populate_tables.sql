f7e699f981ee37cae598c952744f0d9480e8e1ed
Astha <102377770+asthapatel1125@users.noreply.github.com>
2022-11-13 23:15:58 -0500

    Create populate_tables.sql

---
+++ populate_tables.sql
@@ -0,0 +1,24 @@
+INSERT INTO mlt VALUES (01, 'minions');
+INSERT INTO mlt VALUES (02, 'minions');
+INSERT INTO mlt VALUES (03, 'minions');
+INSERT INTO mlt VALUES (04, 'minions');
+INSERT INTO mlt VALUES (05, 'minions');
+
+INSERT INTO doctor VALUES (01, 'minions');
+INSERT INTO doctor VALUES (02, 'minions');
+INSERT INTO doctor VALUES (03, 'minions');
+INSERT INTO doctor VALUES (04, 'minions');
+INSERT INTO doctor VALUES (05, 'minions');
+
+INSERT INTO nurse VALUES (01, 'minions');
+INSERT INTO nurse VALUES (02, 'minions');
+INSERT INTO nurse VALUES (03, 'minions');
+INSERT INTO nurse VALUES (04, 'minions');
+INSERT INTO nurse VALUES (05, 'minions');
+
+
+/*
+
+    MORE TO BE UPDATED
+
+*/
